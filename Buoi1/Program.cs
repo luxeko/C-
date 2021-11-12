8d7aaa6ce2cd0ce34b2b0cdf9030b3a5ac343f9b
Lê Khả Đức Anh <ducanhlekha2802@gmail.com>
2021-11-12 20:04:02 +0700

    create pj c#

---
+++ Buoi1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Buoi1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = "Hello low";
+
+            Console.WriteLine(s);
+        }
+    }
+}
